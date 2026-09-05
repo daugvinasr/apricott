469583a09b2808dff99f24be4e18f16697a6004f
Daugvinas <daugvinasr@gmail.com>
2026-09-05 16:16:56 +0300

    add missing images (#24)

--- src/components/renders.ts
+++ src/components/renders.ts
@@ -5,8 +5,10 @@ type ModelName = (typeof MODEL_NAMES)[ModelId];
 const RENDERS = new Map<ModelName, string>([
   ["GHERO", "/mouseGhero.png"],
   ["G23", "/mouseG23.png"],
-  ["G23V2", "/mouseG23V2.png"],
   ["G24", "/mouseG24.png"],
+  ["G23V2", "/mouseG23V2.png"],
+  ["029", "/mouseG29.png"],
+  ["039", "/mouseG39.png"],
 ]);
 
 export const LINEUP = [...RENDERS].map(([name, src]) => ({ name, src }));
